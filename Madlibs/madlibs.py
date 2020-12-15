8e34380e560c87a53853a8798342b2fa9c9e92ae
MathSena <MathSena07@hotmail.com>
2020-12-14 23:18:07 -0300

    Subindo projeto Guess_Number

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -12,6 +12,6 @@ verb2 = input("Verbo2: ")
 famoso = input("Pessoa Famosa: ")
 
 madlib = f"Programar é tão...{adj}! Me faz tão bem pois \
-    eu amo {verb1}. Continue focado e continue {verb2} pois assim você será como {famoso}!"
+    eu amo {verb1}. Continue focado e continue {verb2} pois nesse caminho, assim você será como {famoso}!"
 
 print(madlib)
