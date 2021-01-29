d8acf0674564d430872ecf9a77c0fd9c9090bfbe
MathSena <MathSena07@hotmail.com>
2021-01-29 00:12:31 -0300

    Continuação

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -16,9 +16,33 @@ class TicTacToe:
 
         def available_moves(self):
             return[i for i, spot in enumerate(self.board) if spot == ' ']
-            #moves = []
-            #for(i,spot) in enumerate(self.board):
-            #    ['x', 'x', 'o'] --> [(0, 'x'), (2,'o')]
-            #    if spot == ' ':
-            #        moves.append(i)
-            #    return moves
+
+        def empty_squares(self):
+            return ' ' in self.board       
+
+        def num_empty_squares(self):
+            return self.board.count(' ')   
+
+        def make_move(self, square, letter):
+            if self.board[square] == ' ':
+                self.board[square] = letter
+                return True
+            return False
+
+
+def play(game, x_player, o_Player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+    letter = 'X'
+
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_Player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f'makes a move to square {square}')
+                game.print_board()
+                print('')
--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -13,14 +13,27 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
 
 class RandomComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9)')
+
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError:
+                    valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+            
+        return val
 
     
         
